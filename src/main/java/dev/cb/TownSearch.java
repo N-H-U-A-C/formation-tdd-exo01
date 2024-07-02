193eaa08c0de72f219e916f4e79f9f2ed313752e
cedric <cedric.belot.dev@gmail.com>
2024-07-02 16:58:50 +0200

    feat/test: work in progress

--- src/main/java/dev/cb/TownSearch.java
+++ src/main/java/dev/cb/TownSearch.java
@@ -2,10 +2,16 @@ package dev.cb;
 
 import jdk.jshell.spi.ExecutionControl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TownSearch {
     private List<String> towns;
+
+    public TownSearch() {
+        towns = Arrays.asList("Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
+    }
 
     public List<String> search(String criteria) {
         if (criteria.length() < 2) throw new NotFoundException("test");
--- src/test/java/dev/cb/TownSearchTest.java
+++ src/test/java/dev/cb/TownSearchTest.java
@@ -1,7 +1,11 @@
 package dev.cb;
 
 import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
@@ -10,10 +14,14 @@ public class TownSearchTest {
     private TownSearch classUnderTest;
     private String criteria;
 
+    @BeforeEach
+    public void setUp() {
+        classUnderTest = new TownSearch();
+    }
+
     @Test
     public void should_Throw_Not_Found_Exception_When_Criteria_Size_Is_2() {
         // given
-        classUnderTest = new TownSearch();
         criteria = "a";
 
         // when
@@ -21,5 +29,18 @@ public class TownSearchTest {
 
         // then
         assertThat(throwable).isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    public void towns_Should_Contain_Valence_And_Vancouver_When_Criteria_Is_Va() {
+        // given
+        criteria = "Va";
+        List<String> expected = Arrays.asList("Valence", "Vancouver");
+
+        // when
+        List<String> result = classUnderTest.search(criteria);
+
+        // then
+        assertThat(result.contains(expected)).isTrue();
     }
 }
