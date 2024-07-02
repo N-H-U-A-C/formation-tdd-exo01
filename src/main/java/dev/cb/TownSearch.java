880af8553d0460a30c83a3298c5adf44369071b8
cedric <cedric.belot.dev@gmail.com>
2024-07-02 15:21:36 +0200

    feat: create TownSearch class

---
+++ src/main/java/dev/cb/TownSearch.java
@@ -0,0 +1,13 @@
+package dev.cb;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.List;
+
+public class TownSearch {
+    private List<String> towns;
+
+    public List<String> search(String criteria)  {
+        throw new ExecutionControl.NotImplementedException();
+    }
+}
