428e07664b98795d974460456605cb93a154b2fd
cedric <cedric.belot.dev@gmail.com>
2024-07-03 10:02:07 +0200

    perf: optimize imports for TownSearchTest class

--- src/test/java/dev/cb/TownSearchTest.java
+++ src/test/java/dev/cb/TownSearchTest.java
@@ -1,13 +1,13 @@
 package dev.cb;
 
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 public class TownSearchTest {
 
